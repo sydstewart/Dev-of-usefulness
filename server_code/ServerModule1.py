a52e87f09cd73e16623bec3b2fe1e7adc1efd92c
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2024-03-08 12:50:19 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -8,8 +8,12 @@ import pandas as pd
 @anvil.server.callable
 def get_change_note_data():
     #read in 10000 rows of data 
-    data = app_tables.change_notes.search()
-    df = pd.DataFrame(data)
-    
-    #print the first 5 rows of the dataframe
-    print(df.head())
+    print('Syd')
+    dicts = app_tables.change_notes.search(classid ='Improvement')
+    df = pd.DataFrame.from_dict(dicts)
+    for column_headers in df.columns: 
+        print(column_headers)
+    # dfimprove = df.groupby(['classid']).count()
+    # print(dfimprove)
+    # #print the first 5 rows of the dataframe
+    # print(df.head())
