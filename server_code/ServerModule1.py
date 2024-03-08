319165ad159cf3c7489609dd80aef29276476389
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2024-03-08 13:05:29 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -9,11 +9,11 @@ import pandas as pd
 def get_change_note_data():
     #read in 10000 rows of data 
     print('Syd')
-    dicts = app_tables.change_notes.search(classid ='Improvement')
+    dicts = app_tables.change_notes.search()
     df = pd.DataFrame.from_dict(dicts)
-    for column_headers in df.columns: 
-        print(column_headers)
+    # for column_headers in df.columns: 
+    #     print(column_headers)
     # dfimprove = df.groupby(['classid']).count()
     # print(dfimprove)
     # #print the first 5 rows of the dataframe
-    # print(df.head())
+    print(df.head())
