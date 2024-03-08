1b419fc22be7e22edb0e4fabdffa3f8427e9a145
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2024-03-08 13:12:00 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -3,17 +3,24 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 import pandas as pd
-
+import plotly.graph_objects as go
 
 @anvil.server.callable
 def get_change_note_data():
     #read in 10000 rows of data 
     print('Syd')
     dicts = app_tables.change_notes.search()
-    df = pd.DataFrame.from_dict(dicts)
-    # for column_headers in df.columns: 
-    #     print(column_headers)
-    # dfimprove = df.groupby(['classid']).count()
-    # print(dfimprove)
-    # #print the first 5 rows of the dataframe
-    print(df.head())
+    # df = pd.DataFrame.from_dict(dicts)
+    # # for column_headers in df.columns: 
+    # #     print(column_headers)
+    # # dfimprove = df.groupby(['classid']).count()
+    # # print(dfimprove)
+    # # #print the first 5 rows of the dataframe
+    # print(df.head())
+
+   # chart_data = app_tables.completed_work.search()
+   dicts = [{'change_date': r['change_date'], 'Class': r['classid']}
+         for r in chart_data]
+   df = pd.DataFrame.from_dict(dicts)
+   print('df',df)
+   line_plots = go.Scatter(x=df['Date_entered'], y=df['delta_work'], name='Delta Work Completed', marker=dict(color='#e50000'))
