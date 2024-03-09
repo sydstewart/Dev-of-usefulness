28efc1644048220e1434dfb274e35f092d3371ec
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2024-03-09 08:28:04 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -15,20 +15,22 @@ def get_change_note_data():
   
     dicts = [{'change_date': r['change_date'], 'Class': r['classid']} for r in changes]
     df = pd.DataFrame.from_dict(dicts)
-    df["change_Date"] = pd.to_datetime(df["change_date"])
+    df['change_date'] = pd.to_datetime(df['change_date'], infer_datetime_format=True, utc=True )
+    # df['change_date'] = df['local_date'].dt.tz_localize('UTC')
+    # df["change_Date"] = pd.to_datetime(df["change_date"])
     # df['date'] = df['change_date'].dt.strftime('%Y/%m')
     print(df)
    # line_plots = go.Scatter(x=df['Date_entered'], y=df['delta_work'], name='Delta Work Completed', marker=dict(color='#e50000'))
     # df.index = pd.to_datetime(change_date.df_cdc.index, utc=True)
     # df1["Change_Date"] = pd.to_datetime(df["change_date"])
     # print(df1)
-    df['year'] = df['change_date'].str[1:7]
+    # df['year'] = df['change_date'].str[1:7]
     # df['change_date'] = pd.to_datetime(df['change_date'])
-    # # df['Year'] = df['change_date'].dt.to_period('Y')
+    df['Year'] = df['change_date'].dt.to_period('Y')
     # # df['date'] = pd.to_datetime(df['change_date'])
     # # df['year'], df['month'] = df['date'].dt.year, df['date'].dt.month
     # # df['yyyy'] = pd.to_datetime(df['change_date']).dt.year (utc=True)
     # # # df['mm'] = pd.to_datetime(df['change_date']).dt.month
       
-    # df = df.groupby('Class').count()
+    df = df.groupby('Year').count()
     print('df',df)
